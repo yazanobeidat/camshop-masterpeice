4bc715761688d2a219c9ef8c744cfa1d77cb3e4d
11-ayaalsawa <ayaalsawa279@gmail.com>
2022-05-21 18:45:34 +0300

    project5

--- index.php
+++ index.php
@@ -145,7 +145,7 @@ if($result_check4 > 0){
                                     session_destroy();
                                     echo'<script>
                                      setTimeout(() => {
-                                         window.location = "home.php";
+                                         window.location = "index.php";
                                        })
                                      
                                      </script>';
@@ -416,72 +416,7 @@ if($result_check4 > 0){
                     <a href="./ProductsPage/Dress.php"><button class="btn1">
                         Click for more
                     </button></a>
-=======
-  <!-- aya -->
-    <nav class="navbar navbar-expand-lg t">
-        <div class="container">
-          <a class="navbar-brand" href="#">Luxury</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav m-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active"  href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Admin</a>
-              </li>
-              <li class="nav-item ">
-                <a class="nav-link " href="#" >
-                  Dropdown
-                </a>
-               
-              </li>
-              <li class="nav-item">
-                <a class="nav-link " href="#" >Disabled</a>
-              </li>
-            </ul>
-          
-          </div>
-        </div>
-      </nav>
 
-      <section class="main">
-          <div class="container py-5">
-              <div class="row py-4">
-                  <div class="col-lg-7 pt-5 text-center">
-                      <h1 class="pt-5">Luxury is in each detail.</h1>
-                  </div>
-              </div>
-          </div>
-      </section>
-
-    <!-- ============================================= -->
-    <section class="category">
-        <div class="container py-5">
-            <div class="row pt-5">
-                <div class="col-lg-7 m-auto">
-                    <div class="row">
-                        <div class="col-lg-3" id="img1">
-                         <img src="./img/project img/cat1.jpg" class="img_luid" alt="">
-                            <h5>DRESSES</h5>
-
-                        </div>
-                        <div class="col-lg-3" id="img2">
-
-                        </div>
-                        <div class="col-lg-3" id="img3">
-
-                        </div>
-                        <div class="col-lg-3" id="img4">
-
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </div>
     </section>
 
 
